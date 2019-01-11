53ccc33628a19a1b27ffcf3318e1fe63ba54afb4
SUDARSHANSHARMA1998 <sudarshansharmano.1@gmail.com>
2019-01-11 15:21:42 +0530

    BufferedReader

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,19 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String args[])
-    {
-        System.out.println("Hello ,World");
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader
+                (new InputStreamReader(System.in));
+        System.out.println("enter your name");
+        String name = br.readLine();
+        System.out.println(name);
+        StringBuffer word = new StringBuffer(name);
+        word.setCharAt(0, 'e');
+        System.out.println(word);
     }
 }
